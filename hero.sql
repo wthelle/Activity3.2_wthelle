d7fbe923359a27854e3fe1408adf877524d9d3a9
unknown <purcia2204@gmail.com>
2023-11-07 12:05:59 +0800

    feat select active players

--- hero.sql
+++ hero.sql
@@ -166,3 +166,9 @@ WHERE hero_id = 1;
 
 DELETE FROM items
 WHERE hero_id = 1;
+
+--5 
+SELECT P.player_name, H.hero_name
+FROM public.player AS P
+INNER JOIN public.hero AS H ON P.hero_id = H.hero_id
+WHERE H.is_active = true;
